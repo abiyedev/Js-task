04e0f652469c71936dd3f6329248eff72c824e26
abiyedev <ayebioweiprecious@gmail.com>
2020-03-26 22:36:32 -0700

    js-task

---
+++ script.js
@@ -0,0 +1,7 @@
+let myName = "Precious"
+let courses = ["HTML", "CSS", "JavaScript", "UI/UX"];
+console.log(myName);
+console.log(courses);
+for (let i = 1; i<=200; i++){
+    if (i % 2 ==0)console.log(i);
+}
